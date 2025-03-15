4c581dc2fec3452e6add68d3265aae066d1383ce
yaho-ma <dev76913@gmail.com>
2025-03-15 09:32:50 +0200

    database connction added

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.sql import text
+from db_setup import get_db
+
+app = FastAPI()
+
+@app.get("/")
+async def read_root(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(text("SELECT 1"))
+    row = result.scalar()
+    return {"status": "Database connected", "result": row}
+    
